c02e08c83094d5c498fb79ee22dd9bf955b6490f
Odilon Relucio <odilon@intelimina.com>
2025-06-28 12:44:46 +0800

    Add enums and validations to Violation model

--- app/models/violation.rb
+++ app/models/violation.rb
@@ -1,2 +1,20 @@
 class Violation < ApplicationRecord
+  enum default_sanction: {
+    verbal_warning: "Verbal Warning",
+    written_warning: "Written Warning",
+    suspension: "Suspension",
+    termination: "Termination"
+  }
+
+  enum severity_level: {
+    minor: "Minor",
+    major: "Major",
+    grave: "Grave"
+  }
+
+  validates :name, presence: true, uniqueness: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "default_sanction", "description", "id", "id_value", "name", "number_of_offenses_strike", "severity_level", "status", "updated_at"]
+  end
 end
