078808a61012eb2f9e682f3917a331a92a6f0127
Odilon Relucio <odilon@intelimina.com>
2025-06-28 13:47:53 +0800

    Refactor AdminUser model to enhance authentication logic and improve login handling

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,12 +1,26 @@
 class AdminUser < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, 
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable,
+        authentication_keys: [:login]
+
+  attr_writer :login
+
+  def login
+    @login || self.username || self.email
+  end
 
   belongs_to :role, optional: true
 
   def self.ransackable_attributes(auth_object = nil)
     ["created_at", "email", "id", "id_value", "updated_at", "username", "first_name", "middle_name", "last_name", "role_id"]
+  end
+
+  # Allow login via username or email
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if (login = conditions.delete(:login))
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { value: login.downcase }]).first
+    else
+      where(conditions.to_h).first
+    end
   end
 end
