f09b6ce8bee8ff35ca03894e08a326f4bd8eaa79
Odilon Relucio <odilon@intelimina.com>
2025-06-28 13:07:54 +0800

    Add holiday_type enum and update ransackable_attributes in Holiday model

--- app/models/holiday.rb
+++ app/models/holiday.rb
@@ -1,4 +1,14 @@
 class Holiday < ApplicationRecord
+  enum holiday_type: {
+    regular_holiday: "Regular Holiday",
+    special_non_working_holiday: "Special Non-Working Holiday",
+    special_working_holiday: "Special Working Holiday"
+  }
+
   validates :name, presence: true
   validates :date, presence: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "date", "description", "holiday_type", "id", "id_value", "is_recurring", "name", "updated_at"]
+  end
 end
