41718010516e2f5e648cc3dbd1791303cbdf3451
Odilon Relucio <odilon@intelimina.com>
2025-06-28 14:32:03 +0800

    Add ransackable_attributes method to Shift model for enhanced search capabilities

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -3,4 +3,7 @@ class Shift < ApplicationRecord
 
   DAYS_OF_WEEK = %w[Sunday Monday Tuesday Wednesday Thursday Friday Saturday].freeze
   
+  def self.ransackable_attributes(auth_object = nil)
+    ["break_end_time", "break_start_time", "created_at", "grace_period", "id", "id_value", "name", "time_in", "time_out", "updated_at", "working_days"]
+  end
 end
