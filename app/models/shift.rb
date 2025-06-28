a2cdc166a29a2a125fe2b8d714c4516f467d12d6
Odilon Relucio <odilon@intelimina.com>
2025-06-28 14:28:04 +0800

    Refactor Shift model to improve formatting and maintain consistency

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,3 +1,6 @@
 class Shift < ApplicationRecord
-  validates :name, presence: true, uniqueness: true  
+  validates :name, presence: true, uniqueness: true
+
+  DAYS_OF_WEEK = %w[Sunday Monday Tuesday Wednesday Thursday Friday Saturday].freeze
+  
 end
