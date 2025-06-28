92b0cd67a7e2be32445661e820b776f65c0a00eb
Odilon Relucio <odilon@intelimina.com>
2025-06-28 13:24:47 +0800

    Add optional role association to AdminUser model

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -4,6 +4,8 @@ class AdminUser < ApplicationRecord
   devise :database_authenticatable, 
          :recoverable, :rememberable, :validatable
 
+  belongs_to :role, optional: true
+
   def self.ransackable_attributes(auth_object = nil)
     ["created_at", "email", "encrypted_password", "id", "id_value", "remember_created_at", "reset_password_sent_at", "reset_password_token", "updated_at"]
   end
