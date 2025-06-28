c3ecb01d86f12f963cb4feb267c108c45321bf3f
Odilon Relucio <odilon@intelimina.com>
2025-06-28 12:55:50 +0800

    Refactor ActiveAdmin registration for Violation model: update form select options and enhance index display

--- app/admin/violations.rb
+++ app/admin/violations.rb
@@ -3,12 +3,30 @@ ActiveAdmin.register Violation do
   permit_params :name, :description, :severity_level, :default_sanction,
                 :number_of_offenses_strike, :status
 
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :description
+    column :severity_level do |violation|
+      violation.severity_level&.humanize
+    end
+    column :default_sanction do |violation|
+      violation.default_sanction&.humanize
+    end
+    column :number_of_offenses_strike
+    column :status
+    column :created_at
+    column :updated_at
+    actions
+  end
+
   form do |f|
     f.inputs do
       f.input :name
       f.input :description, as: :text, input_html: { rows: 8 }
-      f.input :severity_level, as: :select, collection: Violation.severity_levels.values
-      f.input :default_sanction, as: :select, collection: Violation.default_sanctions.values
+      f.input :severity_level, as: :select, collection: Violation.severity_levels.invert
+      f.input :default_sanction, as: :select, collection: Violation.default_sanctions.invert
       f.input :number_of_offenses_strike
       f.input :status
     end
